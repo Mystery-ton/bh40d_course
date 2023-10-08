5e30cb4f13e76ed30ce31a0ec0e8db409d4d4145
Mystery-ton <sedenevskiy@mail.ru>
2023-10-08 20:11:10 +0300

    new branch and some code

--- main.py
+++ main.py
@@ -11,3 +11,20 @@ def hello():
 
 
 print("hello")
+
+text = "Hello"
+print(text.istitle())
+
+text = input("Enter sentence: ")
+
+text2 = text.split()
+text3 = text2[2] + " " + text2[1] + " " + text2[0]
+print(text3)
+
+first_space_index = text.find(" ")
+first_word = text[:first_space_index]
+last_space_index = text.rfind(" ")
+last_word = text[last_space_index + 1 :]
+center = text[first_space_index : last_space_index + 1]
+result = last_word + center + first_word
+print(result)

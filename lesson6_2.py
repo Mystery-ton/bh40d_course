fa7a3dd7a26aebe36bcf4ad02be1c5ec801b4c98
Mystery-ton <sedenevskiy@mail.ru>
2023-10-17 19:39:06 +0300

    6_2 solved

---
+++ lesson6_2.py
@@ -0,0 +1,17 @@
+# Код Морзе для представления цифр и букв использует тире и точки. Напишите
+# функцию для кодирования текстового сообщения в соответствии с кодом Морзе.
+# (словари в помощь
+
+azbuka_morze = {"a": ".-", "b": "-...", "c": "-.-.", " ": " "}
+
+
+def translate(text):
+    res_list = []
+    for i in text:
+        res_list.append(azbuka_morze[i])
+    res = " ".join(res_list)
+    return res
+
+
+text = input("Enter text: ")
+print(translate(text))

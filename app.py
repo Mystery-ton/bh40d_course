8522e7419fdc0a61fffeb4f4b47685df3cca60c6
Mystery-ton <sedenevskiy@mail.ru>
2023-10-24 22:34:45 +0300

    8_1 was solved

--- app.py
+++ app.py
@@ -92,3 +92,126 @@
 #     return number
 #
 # print(factorial(5))
+# def descending_order(num):
+#     ans = str(num)
+#     # ans.sort(key=int, reverse=True)
+#     ans = int("".join(sorted(ans, reverse=True)))
+#     return ans
+#
+#
+# print(descending_order(123))
+# str = "Ffpjj"
+# print(len(str))
+# print(len(set(str)))
+# print
+# filter()
+
+
+# def solution(s):
+#     s = list(s)
+#     print(s)
+#     i = 0
+#     while i in range(0, len(s)):
+#         print(s[i])
+#         if s[i].isupper():
+#             s.insert(i, " ")
+#             i += 1
+#         i += 1
+#     return "".join(s)
+#
+#
+# print(solution("helloWorld"))
+
+
+# def cakes(recipe, available):
+#     cake = 0
+#     # possible = True
+#     while not ("no" in available.values()):
+#         for i in recipe:
+#             print(i)
+#             if i in available and available[i] >= recipe[i]:
+#                 available[i] = available[i] - recipe[i]
+#                 if available[i] <= 0:
+#                     available[i] = "no"
+#             else:
+#                 # if available[i] <= 0 or not (i in available):
+#                 available[i] = "no"
+#                 cake -= 1
+#                 break
+#         cake += 1
+#     return cake
+#
+#
+# print(
+#     cakes(
+#         {"flour": 500, "sugar": 200, "eggs": 1},
+#         {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200},
+#     )
+# )
+# d = {"flour": 500, "sugar": 200, "eggs": 1}
+# d2 = {"flour": 1200, "sugar": 1200, "milk": 200, "eggs": 5}
+# print(d.values())
+# print(d2.values())
+# dict.get()
+# int.bit_count()
+
+
+class Product:
+    """Класс представления товара"""
+
+    def __init__(self, name: str, descr: str, price: float | int):
+        """Object atributes initialization
+
+        :param name: Название товара
+        :param descr: Описание товара
+        :param price: Цена товара
+        """
+        if not isinstance(name, str):
+            raise ValueError
+        if not isinstance(descr, str):
+            raise ValueError
+        if not isinstance(price, (float, int)):
+            raise TypeError
+        if price < 0:
+            raise ValueError
+
+        self.name = name
+        self.descr = descr
+        self.price = price
+
+    def dump(self):
+        """Сериализация объекта
+
+        :return: Словарь с данными объекта
+        """
+        return {"name": self.name, "descr": self.descr, "price": self.price}
+
+    def __str__(self) -> str:
+        return f"{self.name},{self.price},{self.descr}"
+
+    # def __bool__(self):
+    #     return bool(self.price)
+
+    def __len__(self):
+        return round(self.price)
+
+    def __getitem__(self, item):
+        return self.name[item]
+
+    # def iter(self):
+    #     return self
+    #
+    # def __next__(self):
+    #     self.i+=1
+    #     if self.i<len(self.name):
+    #         return self.name[self.i]
+    #     else:
+    #         self.i =-1
+    #         raise StopIteration
+
+    def __eq__(self, other):
+        if isinstance(other, Product):
+            return self.price == other.price
+        elif isinstance(other, (int, float)):
+            return self.price == other
+        raise TypeError
---
+++ lesson8_1.py
@@ -0,0 +1,31 @@
+class Car:
+    """Класс представления автомобиля"""
+
+    def __init__(self, color: str, count_passenger_seats: int, is_baby_seat: bool):
+        """
+        Инициализация атрибутов объекта
+
+        :param color:
+        :param count_passenger_seats:
+        :param is_baby_seat:
+        """
+        if not isinstance(color, str):
+            raise ValueError
+        if not isinstance(count_passenger_seats, int):
+            raise ValueError
+        if not isinstance(is_baby_seat, bool):
+            raise TypeError
+
+        self.color = color
+        self.count_passenger_seats = count_passenger_seats
+        self.is_baby_seat = is_baby_seat
+        self.is_busy = False
+
+    def __str__(self) -> None:
+        print(
+            f"Color is {self.color}, seats: {self.count_passenger_seats}, {'Car has baby seat ' if self.is_baby_seat==True else 'Car hasnt babyseat'}, {self.is_busy} "
+        )
+
+
+car1 = Car(color="red", count_passenger_seats=5, is_baby_seat=True)
+print(car1.__str__())
